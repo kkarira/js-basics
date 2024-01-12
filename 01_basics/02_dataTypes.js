fa710b575ed7b55df41c102731c79d1898632f08
kkarira <42875693+kkarira@users.noreply.github.com>
2024-01-12 16:19:30 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict" // treat all JS code as newer version
+
+//alert("hello")// we are using node-js not browser so alert is not working right now
+
+let name = "kunal"
+let age = 18
+let isLoggedIn = false
+
+// number => 2^53
+// bigInt =>
+// string => ""
+// boolean => true/false
+// null => standalone value or it is an empty value
+// undefined => value not defined
+// symbol => uniqueness identify krne ke liye use hote hain symbols
+
+
+// object
+
+console.log(typeof null) // iska type object aata hai
+console.log(typeof undefined); // iska type undefined aata hai
+
