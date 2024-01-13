d45f024a014a9a56f2609654804e58c9afe955ab
kkarira <42875693+kkarira@users.noreply.github.com>
2024-01-13 15:27:00 +0000

    dd

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,49 @@
+let score = false
+/*
+console.log(typeof score);
+console.log(typeof(score));
+
+let c= valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+*/
+
+/*
+   CONVERSION FROM ANY DATATYPE TO 'NUMBER' DATATYPE
+"33" => 33
+"33abc" => NaN
+true => 1, false => 0
+*/
+
+/*
+    CONVERSION FROM ANY DATATYPE TO 'BOOLEAN' DATATYPE
+1 => TRUE, 0 => FALSE
+"" => FALSE
+"kunal" => true
+*/
+
+/*
+    CONVERSION FROM ANY DATATYPE TO 'STRING' DATATYPE
+
+*/
+
+
+// ************************************************** Operations ***********************************************************
+
+/************************************************************** SUMMARY ****************************************************************** */
+/* 
+PRIMITIVE DATATYPE (call by value)=> 1. String             }
+                                     2. Number             }
+                                     3. Boolean            }
+                                     4. null               }==> uses stack memory             
+                                     5. undefined          }
+                                     6. Symbol             }
+                                     7. BigInt             }
+REFERENCE TYPE (non-primitive)=> 1. Arrays                      }
+                                 2. Objects                     } ==> uses heap memory
+                                 3. Functions                   }
+
+                                 
+
+
+*/
