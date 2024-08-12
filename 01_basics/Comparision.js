9dc427e3ebc677f32d6306e1cf8b1c6ef38f39ec
kkarira <42875693+kkarira@users.noreply.github.com>
2024-08-12 16:44:01 +0000

    datatypes

---
+++ 01_basics/Comparision.js
@@ -0,0 +1,36 @@
+// console.log(2>1);  true
+// console.log(2>=1); true
+// console.log(2<1);  false
+// console.log(2==1); false
+// console.log(2!=1); true
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0); 
+
+console.log("2" === 2);
+
+const score = 100
+const scoreValue = 100.3
+const Loggedin = false
+let userMail;
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+const bigNumber = 3456789643234546433n
+
+const heros = ["shaktiman", "nagraj", "doga"];
+let myObj = {
+    name: "kunal",
+    age: 25,
+}
+
+const myFunc = function(){
+    console.log("in the function");
+}
